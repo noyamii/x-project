2cf87bc8bf8dc757eb594aed797ea3dcb083bfe2
noyamii <batebinim@gmail.com>
2024-12-14 02:22:00 +0330

    post-content

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+
+    public function store (Request $request) {
+        $request->validate([
+            'image' => 'nullable|mimes:jpg,png,jpeg|max:10240',
+            'text' => 'nullable',
+        ]);
+
+        // if both is empty
+        if (($request->image or $request->text) == null){
+            return response('image or text required', 400);
+        }
+
+        $imagePath = '';
+        if ($request->image){
+            $imagePath = time() . $request->file('image')->getClientOriginalName();
+            $request->image->move(public_path('images'), $imagePath);
+            $imagePath = 'image/' . $imagePath;
+        }
+
+
+        Post::create([
+            'text' => $request->text,
+            'image_path' => $imagePath,
+        ]);
+
+        return view('post');
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, 'store']);
