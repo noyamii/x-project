b24d9a4dbc3542a335194e1e7a9855e9885cfd0f
noyamii <batebinim@gmail.com>
2024-12-14 12:03:58 +0330

    creating user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function test(Request $request)
+    {
+        return response($request->cookie());
+    }
+    public function index()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $attriburtes = $request->validate([
+            'username' => 'required',
+            'password' => ['required', Password::min(5)],
+            'name' => 'required',
+        ]);
+
+        $user = User::create($attriburtes);
+
+        Auth::login($user);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'store']);
+Route::post('/post', [PostController::class, 'store']);
+
+Route::post('/user', [UserController::class, 'store']);
+Route::get('/test', [UserController::class, 'test']);
